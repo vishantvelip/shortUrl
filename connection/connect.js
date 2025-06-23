eb68e9529eeca0693b77a0819ef42da33d156a00
Vishantvelip <vishantvelip92@gmail.com>
2025-06-23 17:38:56 +0530

    Update connect.js

--- connection/connect.js
+++ connection/connect.js
@@ -1,16 +1,14 @@
-
 const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
 
-async function connectDB() {
-  try {
-    await mongoose.connect("mongodb://localhost:27017/short-url");
-    console.log("Ok MongoDB connected");
-  } catch (err) {
-    console.error("MongoDB connection error:", err);
-  }
-}
-
-module.exports = {
-    connectDB,
-    
-}
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+      console.log('Server started and connected to MongoDB');
+    });
+  })
+  .catch(err => {
+    console.error('Failed to connect to MongoDB', err);
+    process.exit(1);
+  });
